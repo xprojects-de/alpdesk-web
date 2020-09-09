879c1804a08a8c226202ea273ff689ec2d73ce3b
XProjects B.Hummel <infos@x-projects.de>
2020-09-09 14:04:34 +0200

    Remove Alpdesk-Core because of migrate to seperate Repo

--- alpdesk-core/src/Library/Exceptions/AlpdeskCoreMandantException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Alpdesk\AlpdeskCore\Library\Exceptions;
-
-class AlpdeskCoreMandantException extends \Exception {
-  
-}
--- alpdesk-core/src/Resources/contao/languages/de/tl_alpdeskcore_sessions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$GLOBALS['TL_LANG']['tl_alpdeskcore_sessions']['username'] = array('Benutzername', '');
-$GLOBALS['TL_LANG']['tl_alpdeskcore_sessions']['token'] = array('Token', '');
-
-$GLOBALS['TL_LANG']['tl_alpdeskcore_sessions']['new'] = array('Neue Session', 'Neue Session hinzufügen');
-$GLOBALS['TL_LANG']['tl_alpdeskcore_sessions']['edit'] = array('Session bearbeiten', 'Bearbeitet Session ID %s');
-$GLOBALS['TL_LANG']['tl_alpdeskcore_sessions']['delete'] = array('Session löschen', 'Lösche Session ID %s');
