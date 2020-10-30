7af37c7fefb423f78c1bec35441ed9d074a710d8
XProjects B.Hummel <infos@x-projects.de>
2020-10-30 05:49:12 +0100

    Better Intervals for Mandant

--- alpdesk-automationplugin/src/Controller/AlpdeskAutomationPluginCronController.php
+++ alpdesk-automationplugin/src/Controller/AlpdeskAutomationPluginCronController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Alpdesk\AlpdeskAutomationPlugin\Model\AlpdeskautomationitemsModel;
 use Alpdesk\AlpdeskAutomationPlugin\Model\AlpdeskautomationhistoryModel;
+use Alpdesk\AlpdeskCore\Model\Mandant\AlpdeskcoreMandantModel;
 use Alpdesk\AlpdeskAutomationPlugin\Elements\AlpdeskElementAutomationHistory;
 
 class AlpdeskAutomationPluginCronController extends Controller {
@@ -19,6 +20,19 @@ class AlpdeskAutomationPluginCronController extends Controller {
   public function __construct(ContaoFramework $framework) {
     $this->framework = $framework;
     $this->framework->initialize();
+  }
+
+  private function deleteOldItems($mandant, $limitDays) {
+    if ($limitDays <= 0) {
+      $limitDays = 14;
+    }
+    $constTime = time() - (60 * 60 * 24 * $limitDays);
+    $deleteHistoryItems = AlpdeskautomationhistoryModel::findBy(array('mandant=?', 'tstamp<?'), array($mandant, $constTime));
+    if ($deleteHistoryItems !== null) {
+      foreach ($deleteHistoryItems as $dItem) {
+        $dItem->delete();
+      }
+    }
   }
 
   public function cron(Request $request): Response {
@@ -34,39 +48,49 @@ class AlpdeskAutomationPluginCronController extends Controller {
       if ($dbItems !== null) {
 
         $data = array();
+        $mandantCronIntervals = array();
 
         foreach ($dbItems as $dbItem) {
 
-          if (!array_key_exists($dbItem->mandant, $data)) {
-            $data[$dbItem->mandant] = array();
-          }
+          if (!array_key_exists($dbItem->mandant, $mandantCronIntervals)) {
 
-          $deviceValue = json_decode($dbItem->devicevalue, true);
+            $mandantCronIntervals[$dbItem->mandant] = true;
 
-          if (isset($deviceValue['type'])) {
-
-            $type = intval($deviceValue['type']);
-
-            if ($type == AlpdeskElementAutomationHistory::$TYPE_SENSOR || $type == AlpdeskElementAutomationHistory::$TYPE_TEMPERATURE || $type == AlpdeskElementAutomationHistory::$TYPE_ANALOGIN) {
-
-              $date = date('d.m.Y H:i', intval($dbItem->tstamp));
-              array_push($data[$dbItem->mandant], array(
-                  'tstamp' => $dbItem->tstamp,
-                  'date' => $date,
-                  'devicehandle' => $dbItem->devicehandle,
-                  'devicevalue' => $deviceValue
-              ));
+            $mandantInfo = AlpdeskcoreMandantModel::findById($dbItem->mandant);
+            if ($mandantInfo !== null) {
+              $this->deleteOldItems($mandantInfo->id, $mandantInfo->automationhistorylimit);
+              $mandantHistoryItems = AlpdeskautomationhistoryModel::findBy(array('mandant=?'), array($mandantInfo->id), array('order' => 'tstamp DESC', 'limit' => 1));
+              if ($mandantHistoryItems !== null) {
+                if (($mandantHistoryItems->tstamp + (60 * intval($mandantInfo->automationhistorycroninterval))) > time()) {
+                  $mandantCronIntervals[$dbItem->mandant] = false;
+                }
+              }
             }
           }
-        }
 
-        // Delete old Items
-        // 1 Month => 60sec * 60min = 3600 = 1h * 24 = 1 day * 30 = 30 days
-        $constTime = time() - (60 * 60 * 24 * 30);
-        $deleteHistoryItems = AlpdeskautomationhistoryModel::findBy(array('tstamp<?'), array($constTime));
-        if ($deleteHistoryItems !== null) {
-          foreach ($deleteHistoryItems as $dItem) {
-            $dItem->delete();
+          if ($mandantCronIntervals[$dbItem->mandant] == true) {
+
+            if (!array_key_exists($dbItem->mandant, $data)) {
+              $data[$dbItem->mandant] = array();
+            }
+
+            $deviceValue = json_decode($dbItem->devicevalue, true);
+
+            if (isset($deviceValue['type'])) {
+
+              $type = intval($deviceValue['type']);
+
+              if ($type == AlpdeskElementAutomationHistory::$TYPE_SENSOR || $type == AlpdeskElementAutomationHistory::$TYPE_TEMPERATURE || $type == AlpdeskElementAutomationHistory::$TYPE_ANALOGIN) {
+
+                $date = date('d.m.Y H:i', intval($dbItem->tstamp));
+                array_push($data[$dbItem->mandant], array(
+                    'tstamp' => $dbItem->tstamp,
+                    'date' => $date,
+                    'devicehandle' => $dbItem->devicehandle,
+                    'devicevalue' => $deviceValue
+                ));
+              }
+            }
           }
         }
 
