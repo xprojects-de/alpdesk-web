46b4cb40170b77397f84a15c7efa694ae636bb0a
XProjects B.Hummel <infos@x-projects.de>
2020-11-13 20:44:02 +0100

    Migrate automationplugin to single Repo

--- alpdesk-automationplugin/src/Resources/contao/languages/de/tl_alpdeskcore_mandant.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$GLOBALS['TL_LANG']['tl_alpdeskcore_mandant']['automationhistorycroninterval'] = array('Automation History Cron-Interval', 'Geben sie einen Wert in Minuten ein');
-$GLOBALS['TL_LANG']['tl_alpdeskcore_mandant']['automationhistorylimit'] = array('Automation History Limit', 'Geben sie einen Wert in Tagen ein');
